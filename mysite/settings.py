a808c6e97057158225ee0cdd7b826d740feb2465
moisessdelmoro <moisesdelmoro@gec.inatel.br>
2021-05-27 15:55:58 -0300

    settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,11 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8r$tla8ary8gsf!b!q0sl#9#s@w6xva#sck9knufv7a_04t+hj'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = config('DEBUG', default=True, cast=boll)
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
